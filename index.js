c518295c8e62555504acd48d33fe0623511472ba
zilvinas <zilvis80@mail.ru>
2024-03-13 18:36:59 +0200

    papildymas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const port = 3000;
 
@@ -13,9 +14,7 @@ app.use('/product', productSetterRoutes);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      'mongodb+srv://autodaliugausa:tYwYztux2gWB9Yl1@eshop.no8jkk2.mongodb.net/Eshop?retryWrites=true&w=majority&appName=Eshop'
-    );
+    await mongoose.connect(process.env.MONGO_DB_URI);
     console.log('Database Connected');
   } catch (err) {
     console.log('Error', err.message);
