cd6012a04599805cee6b56d6b65f493f00b9df05
zilvinas <zilvis80@mail.ru>
2024-03-21 22:19:21 +0200

    papildymas

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const productGetterRoutes = require('./routes/productGetterRoutes');
@@ -11,6 +13,7 @@ app.use('/product', productGetterRoutes);
 
 const productSetterRoutes = require('./routes/productSetterRoutes');
 app.use('/product', productSetterRoutes);
+app.get('/', (req, res) => res.send('Express on Vercel'));
 
 const connectDB = async () => {
   try {
